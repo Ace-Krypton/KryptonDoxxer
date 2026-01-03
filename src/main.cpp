37a1fc1eb90cdf588038f3d667204866e55b90c5
Ace-Krypton <ramizna@code.edu.az>
2026-01-03 20:54:13 +0100

    Add main to test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
+#include "collectors/paste_collector.hpp"
+#include "parsers/pattern_scanner.hpp"
+#include "output/json_writer.hpp"
 #include <iostream>
-#include <nlohmann/json.hpp>
 
-auto main() -> int {
-    
+auto main(const int argc, char** argv) -> int {
+    if (argc < 2) {
+        std::cerr << "Usage: kryptondoxxer <url>\n";
+        return 1;
+    }
+
+    const auto text = collect_paste(argv[1]);
+    auto hits = scan_patterns(text);
+    write_json(argv[1], hits);
 }
